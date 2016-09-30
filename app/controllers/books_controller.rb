52bb9e86cc0f6e5bf1739060acd2deb224c74c9f
Ora-Kool <etingemabian@gmail.com>
2016-09-30 14:16:04 +0100

    Added Books and User controller and models

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,28 @@
+class BooksController < ApplicationController
+
+	def index
+		
+	end
+
+	def new
+		
+	end
+
+	def show
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	private
+
+	def books_params
+		
+	end
+end
